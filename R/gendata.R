2a7e9fab4882c52405b1b7cc4cee4908404a5436
JiangshuoZhao <1787076149@qq.com>
2021-12-21 18:30:00 +0800

    parameter modifacation

--- R/gendata.R
+++ R/gendata.R
@@ -2,7 +2,7 @@
 #' @description This function is to generate a matrix with n*p have two class for classification.
 #' @param n The number of sample.
 #' @param p The number of feature dimension.
-#' @param K The true support set size.
+#' @param K The true support set size.Default is 5.
 #' @param Mean The  mean of positive sample, '-Mean' is the mean of negative sample.
 #' @param Sd The standard deviation of sample.
 #' @param sigma standard deviation of noise.
@@ -14,7 +14,7 @@
 #' data <- gendata(1000,100,5)
 #' }
 #' @export
-gendata = function(n, p, K,Mean = 1.5,Sd=0.75, sigma = 0.01, seed = 1234){
+gendata = function(n, p, K=5 ,Mean = 1.5,Sd=0.75, sigma = 0.01, seed = 1234){
   set.seed(seed)
   sigma=0.01
   one=rep(1,n)
